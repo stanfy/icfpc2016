2552220c50577e67774ff3d11e7ba09f6b46eb82
Michael Pustovit <mpustovit@stanfy.com.ua>
2016-08-04 20:15:05 -0700

    Add list snapshots method and retrofit logger

--- src/main/kotlin/icfp16/Main.kt
+++ src/main/kotlin/icfp16/Main.kt
@@ -3,29 +3,36 @@ package icfp16
 import icfp16.api.API_KEY
 import icfp16.api.Api
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 fun main(args: Array<String>) {
   println("ICFP 2016")
 
+  val logInterceptor = HttpLoggingInterceptor()
+  logInterceptor.level = HttpLoggingInterceptor.Level.BODY;
+
+  val okHttpClient = OkHttpClient.Builder()
+      .addInterceptor(logInterceptor)
+      .addInterceptor({ chain ->
+        val request = chain.request().newBuilder()
+            .addHeader("X-API-Key", API_KEY)
+            .build()
+
+        chain.proceed(request)
+      })
+      .build()
+
   val retrofit = Retrofit.Builder()
       .baseUrl("http://2016sv.icfpcontest.org/api/")
       .client(
-          OkHttpClient.Builder()
-              .addInterceptor({ chain ->
-                val request = chain.request().newBuilder()
-                    .addHeader("X-API-Key", API_KEY)
-                    .build()
-
-                chain.proceed(request)
-              })
-              .build()
+          okHttpClient
       )
       .addConverterFactory(GsonConverterFactory.create())
       .build()
 
   val api = retrofit.create(Api::class.java)
 
-  println(api.helloWorld().request())
+  println(api.helloWorld().execute().body())
 }
--- src/main/kotlin/icfp16/api/Api.kt
+++ src/main/kotlin/icfp16/api/Api.kt
@@ -1,16 +1,37 @@
 package icfp16.api
 
+import com.google.gson.annotations.SerializedName
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 val API_KEY = "17-04d569ebce709bf6e5482fea22c8acf0"
 
 interface Api {
   @GET("hello")
   fun helloWorld(): Call<Hello>
+
+  @GET("snapshot/list")
+  fun listSnapchots(): Call<Snapshots>
+
+  // TODO add custom converter for this request
+  @GET("blob/{hash}")
+  fun blob(@Path("hash") hash: String): Call<String>
 }
 
 data class Hello(
     val ok: Boolean,
     val greeting: String
+)
+
+data class Snapshot(
+    @SerializedName("snapshot_time")
+    val time: String,
+    @SerializedName("snapshot_hash")
+    val hash: String
+)
+
+data class Snapshots(
+    val ok: Boolean,
+    val snapshots: List<Snapshot>
 )
