79b08ff1a5d882019276065be81150ebc499bfdf
Vixentael <vixentael@gmail.com>
2016-08-05 21:48:59 +0300

    SequenceSolver with simple states from tasker

--- src/main/kotlin/icfp16/solver/Solver.kt
+++ src/main/kotlin/icfp16/solver/Solver.kt
@@ -1,7 +1,8 @@
 package icfp16.solver
 
 import icfp16.data.*
-import icfp16.estimate.BitmapEstimator
+import icfp16.estimate.CompoundEstimator
+import icfp16.state.PublicStates
 import icfp16.state.State
 
 interface Solver {
@@ -34,20 +35,48 @@ class BetterTranslatorSolver : Solver {
     val vertexes = problem.poligons.flatMap { it.vertices }
     val centroid = massCentroid(vertexes)
 
-    val translation = centroid.add(Vertex(Fraction(-1, 2), Fraction(-1, 2)))
+    val stateCentroid = Vertex(Fraction(-1, 2), Fraction(-1, 2))
+    val translation = centroid.add(stateCentroid)
     val initialSquare = State.initialSquare()
     val translatedSolve = initialSquare.translate(translation)
     return translatedSolve
   }
 }
 
+
+class SequenceSolver: Solver {
+  override fun solve(problem: Problem): State {
+
+    val vertexes = problem.poligons.flatMap { it.vertices }
+    val problemCentroid = massCentroid(vertexes)
+
+    val bestState = PublicStates.states
+      .map { s ->
+        // translate S many times
+        val stateCentroid = massCentroid(s.finalPositions.asList())
+        val translation = problemCentroid.sub(stateCentroid)
+        val translatedState = s.translate(translation)
+        translatedState
+      }
+      .flatMap { s ->
+        // list of all transformations of state
+        listOf<State>(s)
+      }
+      .map { it.to(CompoundEstimator().resemblanceOf(problem, it, quality = 4)) }
+      .sortedBy { it.second }
+      .last().first
+
+    return bestState
+  }
+}
+
 class BestSolverEver: Solver {
 
   override fun solve(problem: Problem): State {
-    val solvers = arrayOf<Solver>(StupidSolver(), TranslatorSolver(), BetterTranslatorSolver())
+    val solvers = arrayOf<Solver>(StupidSolver(), TranslatorSolver(), BetterTranslatorSolver(), SequenceSolver())
     val states =  solvers
         .map { it.solve(problem) }
-        .map { it.to(BitmapEstimator().resemblanceOf(problem, it, quality = 4)) }
+        .map { it.to(CompoundEstimator().resemblanceOf(problem, it, quality = 4)) }
     val sorted = states.sortedBy { it.second }
     return sorted.last().first
   }
