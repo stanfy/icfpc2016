dc8dec7850a40d04b53aee61b4e0820aebff66fd
Roman Mazur <rmazur@stanfy.com.ua>
2016-08-05 11:11:53 -0700

    Add task 2

--- src/main/kotlin/icfp16/Tasker.kt
+++ src/main/kotlin/icfp16/Tasker.kt
@@ -48,6 +48,64 @@ val tasks = arrayOf(
                 Vertex(Fraction(1), Fraction(2, 3))
             )
         )
+    ),
+
+    // Task 1.
+    Pair(
+        Problem(
+            arrayListOf(
+                Polygon(
+                    arrayListOf(
+                        Vertex(Fraction(0), Fraction(1, 3)),
+                        Vertex(Fraction(1, 3), Fraction(1, 3)),
+                        Vertex(Fraction(2, 3), Fraction(2, 3)),
+                        Vertex(Fraction(2, 3), Fraction(1)),
+                        Vertex(Fraction(1, 3), Fraction(1)),
+                        Vertex(Fraction(1, 3), Fraction(2, 3)),
+                        Vertex(Fraction(0), Fraction(2, 3))
+                    )
+                )
+            ),
+            arrayListOf()
+        ),
+        State(
+            arrayOf(
+                Vertex(0, 0),
+                Vertex(Fraction(2, 3), Fraction(0)),
+                Vertex(1, 0),
+                Vertex(Fraction(0), Fraction(1, 3)),
+                Vertex(Fraction(1, 3), Fraction(1, 3)),
+                Vertex(Fraction(1), Fraction(1, 3)),
+                Vertex(Fraction(0), Fraction(2, 3)),
+                Vertex(Fraction(2, 3), Fraction(2, 3)),
+                Vertex(Fraction(1), Fraction(2, 3)),
+                Vertex(0, 1),
+                Vertex(Fraction(1, 3), Fraction(1)),
+                Vertex(1, 1)
+            ),
+            arrayOf(
+                Facet(arrayListOf(0, 1, 4, 3)),
+                Facet(arrayListOf(1, 2, 5, 4)),
+                Facet(arrayListOf(3, 4, 7, 6)),
+                Facet(arrayListOf(4, 5, 8, 7)),
+                Facet(arrayListOf(6, 7, 10, 9)),
+                Facet(arrayListOf(7, 8, 11, 10))
+            ),
+            arrayOf(
+                Vertex(Fraction(0), Fraction(2, 3)),
+                Vertex(Fraction(2, 3), Fraction(2, 3)),
+                Vertex(Fraction(2, 3), Fraction(1)),
+                Vertex(Fraction(0), Fraction(1, 3)),
+                Vertex(Fraction(1, 3), Fraction(1, 3)),
+                Vertex(Fraction(1, 3), Fraction(1)),
+                Vertex(Fraction(0), Fraction(2, 3)),
+                Vertex(Fraction(2, 3), Fraction(2, 3)),
+                Vertex(Fraction(2, 3), Fraction(1)),
+                Vertex(Fraction(0), Fraction(1, 3)),
+                Vertex(Fraction(1, 3), Fraction(1, 3)),
+                Vertex(Fraction(1, 3), Fraction(1))
+            )
+        )
     )
 )
 
