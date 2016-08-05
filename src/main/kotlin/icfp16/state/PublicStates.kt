fa85ed525af6b4054bf42d741e6217c77789c376
zen <zen.dev@yandex.com>
2016-08-05 23:41:05 +0300

    Replace two invalid public states by 1 valid

--- src/main/kotlin/icfp16/state/PublicStates.kt
+++ src/main/kotlin/icfp16/state/PublicStates.kt
@@ -91,83 +91,39 @@ class PublicStates {
         )
       ),
 
-      State(vertexes = arrayOf(
-          Vertex(Fraction(0), Fraction(0)),
-          Vertex(Fraction(1), Fraction(0)),
-          Vertex(Fraction(1), Fraction(1)),
-          Vertex(Fraction(0), Fraction(1)),
-          Vertex(Fraction(0), Fraction(1, 2)),
-          Vertex(Fraction(1, 2), Fraction(1, 2)),
-          Vertex(Fraction(1, 2), Fraction(1))
-      ),
-          facets = arrayOf(
-              Facet(listOf(0, 1, 5, 4)),
-              Facet(listOf(1, 2, 6, 5)),
-              Facet(listOf(3, 4, 5, 3)),
-              Facet(listOf(3, 5, 6, 3))
-          ),
-          finalPositions = arrayOf(
-              Vertex(Fraction(0), Fraction(0)),
-              Vertex(Fraction(1), Fraction(0)),
-              Vertex(Fraction(0), Fraction(0)),
-              Vertex(Fraction(0), Fraction(0)),
-              Vertex(Fraction(0), Fraction(1, 2)),
-              Vertex(Fraction(1, 2), Fraction(1, 2)),
-              Vertex(Fraction(0), Fraction(1, 2))
-          )
-      ),
+        // это свёрнутый в 4 части квадрат.
 
-        // это свёрнутый в 4 части квадрат. можно было проще, но я додумался поздно :(
-      State(vertexes = arrayOf(
-          Vertex(Fraction(0), Fraction(0)), // a
-          Vertex(Fraction(1, 4), Fraction(0)), // b
-          Vertex(Fraction(3, 4), Fraction(0)), // c
-          Vertex(Fraction(1), Fraction(0)), // d
-          Vertex(Fraction(1), Fraction(1, 4)), // e
-          Vertex(Fraction(1), Fraction(3, 4)), // f
-          Vertex(Fraction(1), Fraction(1)), // g
-          Vertex(Fraction(3, 4), Fraction(1)), // j
-          Vertex(Fraction(1, 4), Fraction(1)), // h
-          Vertex(Fraction(0), Fraction(1)), // m
-          Vertex(Fraction(0), Fraction(3, 4)), // n
-          Vertex(Fraction(0), Fraction(1, 4)), // p
-          Vertex(Fraction(1, 4), Fraction(1, 4)), // k
-          Vertex(Fraction(3, 4), Fraction(1, 4)), // o
-          Vertex(Fraction(3, 4), Fraction(3, 4)), // s
-          Vertex(Fraction(1, 4), Fraction(3, 4)) // z
-      ),
-          facets = arrayOf(
-              Facet(listOf(0, 1, 12, 11)),
-              Facet(listOf(1, 2, 13, 12)),
-              Facet(listOf(2, 3, 4, 13)),
-              Facet(listOf(13, 4, 5, 14)),
-              Facet(listOf(13, 4, 5, 14)),
-              Facet(listOf(14, 5, 6, 7)),
-              Facet(listOf(15, 14, 7, 8)),
-              Facet(listOf(10, 15, 8, 9)),
-              Facet(listOf(11, 12, 15, 10)),
-              Facet(listOf(12, 13, 14, 15))
-          ),
-          finalPositions = arrayOf(
-              Vertex(Fraction(1, 2), Fraction(1, 2)), // a //
-              Vertex(Fraction(1, 4), Fraction(1, 2)), // b //
-              Vertex(Fraction(3, 4), Fraction(1, 2)), // c //
-              Vertex(Fraction(1, 2), Fraction(1, 2)), // d //
-              Vertex(Fraction(1, 2), Fraction(1, 4)), // e  //
-              Vertex(Fraction(1, 2), Fraction(3, 4)), // f //
-              Vertex(Fraction(1, 2), Fraction(1, 2)), // g //
-              Vertex(Fraction(3, 4), Fraction(1, 2)), // j //
-              Vertex(Fraction(1, 4), Fraction(1, 2)), // h //
-              Vertex(Fraction(1, 2), Fraction(1, 2)), // m //
-              Vertex(Fraction(1, 2), Fraction(3, 4)), // n //
-              Vertex(Fraction(1, 2), Fraction(1, 4)), // p //
-              Vertex(Fraction(1, 4), Fraction(1, 4)), // k //
-              Vertex(Fraction(3, 4), Fraction(1, 4)), // o //
-              Vertex(Fraction(3, 4), Fraction(3, 4)), // s //
-              Vertex(Fraction(1, 4), Fraction(3, 4)) // z //
-          )
-      )
+        State(vertexes = arrayOf(
+            Vertex(Fraction(0), Fraction(0)),
+            Vertex(Fraction(0), Fraction(1, 2)),
+            Vertex(Fraction(0), Fraction(1)),
+            Vertex(Fraction(1, 2), Fraction(1)),
+            Vertex(Fraction(1), Fraction(1)),
+            Vertex(Fraction(1), Fraction(1, 2)),
+            Vertex(Fraction(1), Fraction(0)),
+            Vertex(Fraction(1, 2), Fraction(0)),
+            Vertex(Fraction(1, 2), Fraction(1, 2))
+        ),
+            facets = arrayOf(
+                Facet(arrayListOf(0, 1, 8, 7)),
+                Facet(arrayListOf(1, 2, 3, 8)),
+                Facet(arrayListOf(8, 3, 4, 5)),
+                Facet(arrayListOf(7, 8, 5, 6))
+            ),
 
+            finalPositions = arrayOf(
+                Vertex(Fraction(0), Fraction(0)), //
+                Vertex(Fraction(0), Fraction(1, 2)), //
+                Vertex(Fraction(0), Fraction(0)), //
+                Vertex(Fraction(1, 2), Fraction(0)), //
+                Vertex(Fraction(0), Fraction(0)), //
+                Vertex(Fraction(0), Fraction(1, 2)),
+                Vertex(Fraction(0), Fraction(0)),
+                Vertex(Fraction(1, 2), Fraction(0)),
+                Vertex(Fraction(1, 2), Fraction(1, 2)) //
+            )
+        )
+        
       /*,
 
       // task 2
