99ae534f10fc04671fb016ad063c0993ade948d8
zen <zen.dev@yandex.com>
2016-08-06 12:58:33 +0300

    Update solving script

--- get-and-solve-new-problems.sh
+++ get-and-solve-new-problems.sh
@@ -2,8 +2,11 @@
 
 echo "Pulling repo.."
 git pull || exit 1
+echo "=> Pulled"
 
+echo "Start grabbing.."
 ./gradlew run -PrunArgs=grab || exit 1
+echo "=> Grabbed"
 
 if ! git diff-index --no-ext-diff --quiet HEAD --; then
     git add parsed_problems || exit 1
@@ -11,13 +14,13 @@ if ! git diff-index --no-ext-diff --quiet HEAD --; then
 	git push || exit 1
 fi
 
+echo "=> Start solving.."
 ./gradlew run -PrunArgs=doit || exit 1
+echo "=> Saving farmed solutions.."
 
 if ! git diff-index --no-ext-diff --quiet HEAD --; then
-    git add parsed_problems || exit 1
-	git commit -m "[AUTO] Add new problems at $(date)" # it may fail if no changes, it's ok
+    git add generated_solutions
+    git add generated_pictures
+	git commit -m "[AUTO] Add farmed solutions at $(date)" # it may fail if no changes, it's ok
 	git push || exit 1
 fi
-
-git commit -m "[AUTO] Farm solved problems at $(date)" # it may fail if no changes, it's ok
-git push || echo "Failed to push new solved problems to the repo"; exit 1
