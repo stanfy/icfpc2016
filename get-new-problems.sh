f5806ad81958dc28de7c6654d438c764a179a808
zen <zen.dev@yandex.com>
2016-08-06 12:34:37 +0300

    Remove attempt to commit during new problems solve to prevent exit with code 1

--- get-new-problems.sh
+++ get-new-problems.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-git pull || exit 1
-./gradlew run -PrunArgs=grab || exit 1
-git add parsed_problems || exit 1
-git commit -m "[AUTO] Add new problems at $(date)" # it may fail if no changes, it's ok
-git push || exit 1
+echo "Pulling repo.."
+git pull || echo "Failed to pull"; exit 1
+
+gw run -PrunArgs=automate
+./gradlew run -PrunArgs=grab || echo "Failed to grab new problems"; exit 1
+
+if ! git diff-index --quiet HEAD --; then
+    git add parsed_problems || echo "Failed to add new problems to the repo"; exit 1
+	git commit -m "[AUTO] Add new problems at $(date)" # it may fail if no changes, it's ok
+	git push || echo "Failed to push grabbed problems to the repo1"; exit 1
+fi
