ae7a94f9257d0a56deaf2683061de42d92940663
zen <zen.dev@yandex.com>
2016-08-06 18:47:27 +0300

    Update solution 18

--- src/main/kotlin/icfp16/state/DimaPublicStates.kt
+++ src/main/kotlin/icfp16/state/DimaPublicStates.kt
@@ -8,28 +8,6 @@ import icfp16.data.Vertex
 
 class DimaPublicStates {
   companion object {
-//    fun lineSolution(folds: Int): IState {
-//      val polygons = (0..folds - 1).map { idx ->
-//        ComplexPolygon(
-//
-//            initial = listOf(
-//                v(f(idx, folds), f(0, 1)),
-//                v(f(idx, folds), f(1, 1)),
-//                v(f(idx + 1, folds), f(1, 1)),
-//                v(f(idx + 1, folds), f(0, 1))
-//            ),
-//            final = listOf(
-//                v(f(idx % 2, folds), f(0, 1)),
-//                v(f(idx % 2, folds), f(1, 1)),
-//                v(f((idx + 1) % 2, folds), f(1, 1)),
-//                v(f((idx + 1) % 2, folds), f(0, 1))
-//            )
-//        )
-//      }
-//      return ComplexState(polygons.toTypedArray())
-//          .appendName("Line(x$folds}")
-//    }
-
     val states: Array<IState> = arrayOf(
         // problem 18
         ComplexState()
@@ -38,9 +16,19 @@ class DimaPublicStates {
                 Vertex(Fraction(0), Fraction(1, 2))
             ))
             .fold(Edge(
-                Vertex(Fraction(1, 2), Fraction(1, 2)),
-                Vertex(Fraction(0), Fraction(1, 4))
+                Vertex(Fraction(1), Fraction(3, 4)),
+                Vertex(Fraction(1, 2), Fraction(1, 2))
             )).apply { name = "problem_18" }
+
+//        ComplexState()
+//            .fold(Edge(
+//                Vertex(Fraction(1), Fraction(1, 2)),
+//                Vertex(Fraction(1, 2), Fraction(0))
+//            ))
+//            .fold(Edge(
+//                Vertex(Fraction(1), Fraction(1, 2)),
+//                Vertex(Fraction(1, 2), Fraction(1))
+//            )).apply { name = "problem_39" }
         )
   }
 }
