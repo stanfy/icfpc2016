56bc9ca0b7377e076db61a92850f95566aa395e1
zen <zen.dev@yandex.com>
2016-08-06 16:52:14 +0300

    Add logging to gen new problems script to be called from launchd

--- get-new-problems.sh
+++ get-new-problems.sh
@@ -1,12 +1,28 @@
 #!/usr/bin/env bash
 
-echo "Pulling repo.."
-git pull || exit 1
+if [ $# -ge 1 ]; then 
+LOG_PATH="$1"
+else
+LOG_PATH="/tmp/icfp_get_new_problem_log.txt"
+fi
 
-./gradlew run -PrunArgs=grab || exit 1
+echo >> $LOG_PATH
+date >> $LOG_PATH
+
+echo "Pulling repo.." >> $LOG_PATH
+git pull >> $LOG_PATH 2>&1 || exit 1
+echo "Pulled"
+
+./gradlew run -PrunArgs=grab >> $LOG_PATH || exit 1
 
 if ! git diff-index --quiet HEAD --; then
+	echo "Going to commit" >> $LOG_PATH
+
     git add parsed_problems || exit 1
 	git commit -m "[AUTO] Add new problems at $(date)" # it may fail if no changes, it's ok
-	git push || exit 1
+	git push >> $LOG_PATH || exit 1
+
+	echo "Commited" >> $LOG_PATH
 fi
+
+echo "Done" >> $LOG_PATH
--- scheduler-get-new-problems.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-while true; do 
-	sh ./get-new-problems.sh
-	sleep 900
-done
