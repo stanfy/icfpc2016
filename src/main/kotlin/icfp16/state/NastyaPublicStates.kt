810f52caa1b32279688196e717893d6de92d1f9e
Vixentael <vixentael@gmail.com>
2016-08-08 01:01:01 +0300

    new square with triangles

--- src/main/kotlin/icfp16/state/NastyaPublicStates.kt
+++ src/main/kotlin/icfp16/state/NastyaPublicStates.kt
@@ -69,7 +69,7 @@ class NastyaPublicStates {
           )
         ).setName("Triangle Three times"),
 
-        // triangle 1/4 of square
+        // square with triangles 1/8
         State(
           arrayOf(
             Vertex(0, 0), // 0
@@ -110,7 +110,50 @@ class NastyaPublicStates {
             Vertex(Fraction(6, 8), Fraction(6, 8)), // 10
             Vertex(Fraction(6, 8), Fraction(2, 8)) // 11
           )
-        ).setName("Almost star, problem #XX")
+        ).setName("Almost star, problem #XX"),
+
+        // square with triangles 1/4
+        State(
+          arrayOf(
+            Vertex(0, 0), // 0
+            Vertex(0, 1), // 1
+            Vertex(1, 1), // 2
+            Vertex(1, 0), // 3
+            Vertex(Fraction(1, 4), Fraction(1, 4)), // 4
+            Vertex(Fraction(1, 4), Fraction(3, 4)), // 5
+            Vertex(Fraction(3, 4), Fraction(3, 4)), // 6
+            Vertex(Fraction(3, 4), Fraction(1, 4)), // 7
+            Vertex(Fraction(0), Fraction(1, 2)), // 8
+            Vertex(Fraction(1, 2), Fraction(1)), // 9
+            Vertex(Fraction(1), Fraction(1, 2)), // 10
+            Vertex(Fraction(1, 2), Fraction(0)) // 11
+          ),
+          arrayOf(
+            Facet(listOf(0, 8, 4)),
+            Facet(listOf(8, 1, 5, 4)),
+            Facet(listOf(1, 9, 5)),
+            Facet(listOf(9, 2, 6, 5)),
+            Facet(listOf(6, 2, 10)),
+            Facet(listOf(6, 10, 3, 7)),
+            Facet(listOf(11, 7, 3)),
+            Facet(listOf(0, 4, 7, 11)),
+            Facet(listOf(4, 5, 6, 7))
+          ),
+          arrayOf(
+            Vertex(Fraction(0), Fraction(1, 2)), // 0
+            Vertex(Fraction(1, 2), Fraction(1)), // 1
+            Vertex(Fraction(1), Fraction(1, 2)), // 2
+            Vertex(Fraction(1, 2), Fraction(0)), // 3
+            Vertex(Fraction(1, 4), Fraction(1, 4)), // 4
+            Vertex(Fraction(1, 4), Fraction(3, 4)), // 5
+            Vertex(Fraction(3, 4), Fraction(3, 4)), // 6
+            Vertex(Fraction(3, 4), Fraction(1, 4)), // 7
+            Vertex(Fraction(1, 2), Fraction(1, 2)), // 8
+            Vertex(Fraction(1, 2), Fraction(1, 2)), // 9
+            Vertex(Fraction(1, 2), Fraction(1, 2)), // 10
+            Vertex(Fraction(1, 2), Fraction(1, 2)) // 11
+          )
+        ).setName("Square with triangles, 1/4, problem #4725")
 
       )
 
