8c806b8ebc1a2b6d6abce5c79eeb9be65a8ce9da
zen <zen.dev@yandex.com>
2016-08-07 16:22:37 +0300

    Update polygons lookup code

--- src/main/kotlin/icfp16/solver/UnfoldingSolver.kt
+++ src/main/kotlin/icfp16/solver/UnfoldingSolver.kt
@@ -19,43 +19,88 @@ class UnfoldingSolver : Solver {
     return initialSquare
   }
 
+  fun filterPolygons(polygons: List<Polygon>): List<Polygon> {
+    return when (polygons.count()) {
+      0 -> emptyList()
+      1 -> polygons
+      else -> {
+        val output = mutableListOf(polygons.first())
+        for (index in 1..polygons.count() - 1) {
+          for (lhs in output) {
+            val rhs = polygons[index]
+            if (!lhs.equalsToPolygon(rhs)) {
+              output.add(rhs)
+            }
+          }
+        }
+
+        output
+      }
+    }
+  }
+
   fun polygonsFromProblem(problem: Problem): List<Polygon> {
     if (problem.skeleton.count() < 3) {
       return emptyList()
     }
 
     val initialEdge = problem.skeleton.first()
-    val polygons = visitEdge(initialEdge, arrayOf(initialEdge), problem.skeleton)
-
-    val uniquePolygons: MutableSet<Polygon> = mutableSetOf()
-    return polygons.filter { uniquePolygons.add(it) }
+    val polygons = visitVertex(initialEdge.a, emptyArray(), problem.skeleton)
+    return filterPolygons(polygons)
   }
 
-  fun findAdjoinedEdges(edge: Edge, visitedEdges: Array<Edge>, potentialEdges: List<Edge>): List<Edge> {
-    return potentialEdges.filter { it != edge && (it.hasPoint(edge.a) || it.hasPoint(edge.b)) && !visitedEdges.reversed().contains(it) }
+  fun findAdjoinedEdges(vertex: Vertex, visitedEdges: Array<Edge>, potentialEdges: List<Edge>): List<Edge> {
+    return potentialEdges.filter { (it.hasPoint(vertex)) && !visitedEdges.contains(it) }
   }
 
-  fun visitEdge(edge: Edge, visitedEdges: Array<Edge>, availableEdges: List<Edge>): List<Polygon> {
-    val adjoinedEdges = findAdjoinedEdges(edge, visitedEdges, availableEdges)
+  fun polygonFromEdges(edges: Array<Edge>): Polygon {
+    val uniqueVertices: MutableSet<Vertex> = mutableSetOf()
+    val polygonVertices: MutableList<Vertex> = mutableListOf()
+
+    edges.forEach {
+      if (uniqueVertices.add(it.a)) {
+        polygonVertices.add(it.a)
+      }
+
+      if (uniqueVertices.add(it.b)) {
+        polygonVertices.add(it.b)
+      }
+    }
+
+    return Polygon(polygonVertices)
+  }
+
+  fun nearestClosingPolygon(initialVertex: Vertex, visitedEdges: Array<Edge>): Polygon {
+    val polygonVertices: MutableList<Vertex> = mutableListOf(initialVertex)
+    var previousVertex = initialVertex
+
+    for (edge in visitedEdges.reversed()) {
+      val currentVertex = if (edge.a == previousVertex) edge.b else edge.a
+
+      if (currentVertex == initialVertex) {
+        return Polygon(polygonVertices)
+      }
+
+      polygonVertices.add(currentVertex)
+      previousVertex = currentVertex
+    }
+
+    // technically it shouldn't be reachable
+    return Polygon(polygonVertices)
+  }
+
+  fun nextVertexForVertex(vertex: Vertex, edge: Edge): Vertex {
+    return if (edge.a == vertex) edge.b else edge.a
+  }
+
+  fun visitVertex(vertex: Vertex, visitedEdges: Array<Edge>, availableEdges: List<Edge>): List<Polygon> {
+    val adjoinedEdges = findAdjoinedEdges(vertex, visitedEdges, availableEdges)
 
     return when (adjoinedEdges.count()) {
       0 -> {
         if (visitedEdges.count() > 3) {
-          // TODO: normalize edges in counterclockwise direction
-          val uniqueVertices: MutableSet<Vertex> = mutableSetOf()
-          val polygonVertices: MutableList<Vertex> = mutableListOf()
-
-          visitedEdges.forEach {
-            if (uniqueVertices.add(it.a)) {
-              polygonVertices.add(it.a)
-            }
-
-            if (uniqueVertices.add(it.b)) {
-              polygonVertices.add(it.b)
-            }
-          }
-
-          listOf(Polygon(polygonVertices))
+          val polygon = nearestClosingPolygon(vertex, visitedEdges)
+          listOf(polygon)
         } else {
           emptyList()
         }
@@ -63,15 +108,18 @@ class UnfoldingSolver : Solver {
 
       1 -> {
         val nextEdge = adjoinedEdges.first()
-        visitEdge(nextEdge, visitedEdges.plus(nextEdge), availableEdges)
+        val nextVertex = nextVertexForVertex(vertex, nextEdge)
+        visitVertex(nextVertex, visitedEdges.plus(nextEdge), availableEdges)
       }
 
       else -> {
         val output: MutableList<Polygon> = mutableListOf()
 
         for (nextEdge in adjoinedEdges) {
-          val polygons = visitEdge(nextEdge, visitedEdges.plus(nextEdge), availableEdges)
-          output.plus(polygons)
+          val nextVertex = nextVertexForVertex(vertex, nextEdge)
+          val polygons = visitVertex(nextVertex, visitedEdges.plus(nextEdge), availableEdges)
+
+          output.addAll(polygons)
         }
 
         output
