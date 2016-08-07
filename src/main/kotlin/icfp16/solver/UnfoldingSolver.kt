28a7da908c6a5e72dfe018b61958598bdc2c96c2
zen <zen.dev@yandex.com>
2016-08-07 12:32:48 +0300

    Add unfolding solver+ test files

---
+++ src/main/kotlin/icfp16/solver/UnfoldingSolver.kt
@@ -0,0 +1,12 @@
+package icfp16.solver
+
+import icfp16.data.Problem
+import icfp16.state.IState
+import icfp16.state.State
+
+class UnfoldingSolver : Solver {
+  override fun solve(problem: Problem, problemId: String, thresholdResemblance: Double): IState {
+    val initialSquare = State.initialSquare()
+    return initialSquare
+  }
+}
---
+++ src/test/kotlin/icfp16/solver/UnfoldingSolverTest.kt
@@ -0,0 +1,114 @@
+package icfp16.solver
+
+import icfp16.api.parseProblem
+import icfp16.estimate.BitmapEstimator
+import icfp16.io.ProblemContainersParser
+import icfp16.visualizer.Visualizer
+import org.assertj.core.api.Assertions
+import org.junit.Ignore
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.Timeout
+import java.util.concurrent.TimeUnit
+
+class UnfoldingSolverTest {
+
+  @Rule
+  fun timeoutRule(): Timeout = Timeout(10, TimeUnit.SECONDS)
+
+  fun differentSolvers() {
+
+    val problemString =
+        """1
+          |4
+          |1,1
+          |2,1
+          |2,2
+          |1,2
+          |4
+          |1,1 1,2
+          |2,1 2,2
+          |1,1 2,1
+          |1,2 2,2
+        """.trimMargin()
+
+    val problem = parseProblem(problemString)
+    val stupidSolve = StupidSolver().solve(problem, "")
+    val translationSolve = TranslatorSolver().solve(problem, "")
+
+    val solve1R = BitmapEstimator().resemblanceOf(problem, stupidSolve!!)
+    val translatorR = BitmapEstimator().resemblanceOf(problem, translationSolve!!)
+
+//    Visualizer().visualizedAndSaveImage(problem, stupidSolve,filePath = "./stupind.png")
+//    Visualizer().visualizedAndSaveImage(problem, translationSolve,filePath = "./translator.png")
+    Assertions.assertThat(stupidSolve).isNotEqualTo(translationSolve)
+    Assertions.assertThat(translatorR).isGreaterThan(solve1R)
+  }
+
+  @Test
+  fun task19Solution() {
+
+    val problemString =
+        """1
+          |12
+          |4/7,3/7
+          |5/7,3/7
+          |13/14,4/7
+          |8/7,4/7
+          |8/7,5/7
+          |1,13/14
+          |1,8/7
+          |6/7,8/7
+          |9/14,1
+          |3/7,1
+          |3/7,6/7
+          |4/7,9/14
+          |16
+          |5/7,3/7 8/7,5/7
+          |3/7,6/7 3/7,1
+          |4/7,3/7 4/7,1
+          |5/7,3/7 5/7,1
+          |6/7,4/7 6/7,8/7
+          |1,4/7 1,8/7
+          |8/7,4/7 8/7,5/7
+          |3/7,6/7 6/7,8/7
+          |4/7,3/7 5/7,3/7
+          |5/7,3/7 3/7,6/7
+          |4/7,4/7 8/7,4/7
+          |4/7,5/7 8/7,5/7
+          |8/7,5/7 6/7,8/7
+          |3/7,6/7 1,6/7
+          |3/7,1 1,1
+          |6/7,8/7 1,8/7
+        """.trimMargin()
+
+    val problem = parseProblem(problemString)
+    val stupidSolve = StupidSolver().solve(problem, "")
+    val translationSolve = TranslatorSolver().solve(problem, "")
+
+    val solve1R = BitmapEstimator().resemblanceOf(problem, stupidSolve!!)
+    val translatorR = BitmapEstimator().resemblanceOf(problem, translationSolve!!)
+
+//    Visualizer().visualizedAndSaveImage(problem, stupidSolve,filePath = "./stupind_19.png")
+//    Visualizer().visualizedAndSaveImage(problem, translationSolve,filePath = "./translator_19.png")
+    Assertions.assertThat(stupidSolve).isNotEqualTo(translationSolve)
+    Assertions.assertThat(translatorR).isGreaterThan(solve1R)
+  }
+
+
+  @Test
+  @Ignore
+  fun task6Solution() {
+
+    val problemString =
+        ProblemContainersParser().generateProblemContainerForProblemId("6")!!.problem.rawString
+
+    val problem = parseProblem(problemString)
+    val translationSolve = SequenceSolver().solve(problem , "123")
+
+    val translatorR = BitmapEstimator().resemblanceOf(problem, translationSolve!!)
+
+    Visualizer().visualizedAndSaveImage(problem, translationSolve,filePath = "./translator_19.png")
+  }
+
+}
